38650fe896e929abcb357c3a9c92bb3fd267a890
zilolaxon <zilolakhon2007@gmail.com>
2023-02-15 21:11:43 +0500

    start

--- app/src/main/java/com/example/customdialoggit/MainActivity.kt
+++ app/src/main/java/com/example/customdialoggit/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.customdialoggit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.customdialoggit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.basicDialog.setOnClickListener {
+            basicDialog()
+        }
+
+
+    }
+
+    private fun basicDialog() {
+
     }
 }
