291c0a579a740e28ad97f5cf6e8d5adf81bcb476
zilolaxon <zilolakhon2007@gmail.com>
2023-02-20 14:12:21 +0500

    start

--- app/src/main/java/com/example/customdialoggit/MainActivity.kt
+++ app/src/main/java/com/example/customdialoggit/MainActivity.kt
@@ -8,7 +8,6 @@ import android.widget.TimePicker
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import com.example.customdialoggit.databinding.ActivityMainBinding
-import com.example.dialoggit.classes.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -16,109 +15,109 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.basicDialog.setOnClickListener {
-            basicDialog()
-        }
-
-        binding.dateDialog.setOnClickListener {
-            dateDialog()
-        }
-
-        binding.colorsheetDialog.setOnClickListener {
-            colorDialog()
-        }
-
-        binding.checkboxDialog.setOnClickListener {
-            openCheckBoxDialog()
-        }
-
-        binding.wifiDialog.setOnClickListener {
-            wifiDialog()
-        }
-
-
-    }
-
-    private fun wifiDialog() {
-        val dialog = WifiDialog()
-        dialog.show(supportFragmentManager, "wifi_dialog")
-        dialog.createDialog("Google wifi", "Pixel 2 A", "i929uui4ui", "Wpa2")
-        dialog.setonButtonsCLickedListener(object : WifiDialog.WiFiDialogListeners {
-            override fun okButtonClicked(password: String) {
-                Toast.makeText(this@MainActivity, "$password", Toast.LENGTH_SHORT).show()
-            }
-            override fun cancelButtonClicked() {}
-        })
+//
+//        binding.basicDialog.setOnClickListener {
+//            basicDialog()
+//        }
+//
+//        binding.dateDialog.setOnClickListener {
+//            dateDialog()
+//        }
+//
+//        binding.colorsheetDialog.setOnClickListener {
+//            colorDialog()
+//        }
+//
+//        binding.checkboxDialog.setOnClickListener {
+//            openCheckBoxDialog()
+//        }
+//
+//        binding.wifiDialog.setOnClickListener {
+//            wifiDialog()
+//        }
 
 
     }
 
-    private fun colorDialog() {
-        val dialog = ColorDialog()
-        dialog.show(supportFragmentManager, "color_dialog")
-        dialog.setOnColorSelectedListener(object : ColorDialog.OnColorSelectListener {
-            override fun OnColorSelected(color: Int) {
-                binding.colorsheetDialog.setBackgroundColor(color)
-            }
-
-        })
-    }
-
-    private fun dateDialog() {
-        val dialog = DateAndTimeDialog()
-        dialog.show(supportFragmentManager, "date_dialog")
-        dialog.setOnOkButtonClicked(object : DateAndTimeDialog.OnButtonClicked {
-            override fun OnOkButtonClicked(datePicker: DatePicker, timePicker: TimePicker) {
-                val date = "${datePicker.dayOfMonth}.${datePicker.month}.${datePicker.year}  " +
-                        "${timePicker.hour}:${timePicker.minute}"
-                Toast.makeText(this@MainActivity, date, Toast.LENGTH_SHORT).show()
-            }
-
-
-        })
-    }
-
-    private fun basicDialog() {
-        val basicDialog = BasicDialog(this)
-        basicDialog.createDialog(
-            title = "Lorem ipsum?",
-            mainTxt = "Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
-                    "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            okBtnTxt = "Agree",
-            cancelBtnTxt = "Disagree"
-        )
-        basicDialog.show(supportFragmentManager, "basic_dialog")
-        basicDialog.setOnButtonClickListener(object : BasicDialog.BasicDialogListeners {
-            override fun okButtonClicked() {
-                Toast.makeText(this@MainActivity, "DISAGREE", Toast.LENGTH_SHORT).show()
-
-            }
-
-            override fun cancelButtonClicked() {
-                Toast.makeText(this@MainActivity, "AGREE", Toast.LENGTH_SHORT).show()
-
-            }
-
-        })
-    }
-
-    private fun openCheckBoxDialog() {
-        val list = arrayListOf("Twitter", "Google", "Instagram", "Facebook")
-
-        val checkBoxDialog = CheckBoxDialog(list, "Social medias")
-        checkBoxDialog.setOnChooseClickListener(object : CheckBoxDialog.OnChooseClickListener {
-            override fun chooseClick(chosenList: List<String>) {
-                val stringBuilder = StringBuilder()
-                for (i in list) {
-                    stringBuilder.append("$i ")
-                }
-                Toast.makeText(this@MainActivity, stringBuilder.toString(), Toast.LENGTH_SHORT)
-                    .show()
-            }
-
-        })
-        checkBoxDialog.show(supportFragmentManager, "checkbox_dialog")
-    }
+//    private fun wifiDialog() {
+//        val dialog = WifiDialog()
+//        dialog.show(supportFragmentManager, "wifi_dialog")
+//        dialog.createDialog("Google wifi", "Pixel 2 A", "i929uui4ui", "Wpa2")
+//        dialog.setonButtonsCLickedListener(object : WifiDialog.WiFiDialogListeners {
+//            override fun okButtonClicked(password: String) {
+//                Toast.makeText(this@MainActivity, "$password", Toast.LENGTH_SHORT).show()
+//            }
+//            override fun cancelButtonClicked() {}
+//        })
+//
+//
+//    }
+//
+//    private fun colorDialog() {
+//        val dialog = ColorDialog()
+//        dialog.show(supportFragmentManager, "color_dialog")
+//        dialog.setOnColorSelectedListener(object : ColorDialog.OnColorSelectListener {
+//            override fun OnColorSelected(color: Int) {
+//                binding.colorsheetDialog.setBackgroundColor(color)
+//            }
+//
+//        })
+//    }
+//
+//    private fun dateDialog() {
+//        val dialog = DateAndTimeDialog()
+//        dialog.show(supportFragmentManager, "date_dialog")
+//        dialog.setOnOkButtonClicked(object : DateAndTimeDialog.OnButtonClicked {
+//            override fun OnOkButtonClicked(datePicker: DatePicker, timePicker: TimePicker) {
+//                val date = "${datePicker.dayOfMonth}.${datePicker.month}.${datePicker.year}  " +
+//                        "${timePicker.hour}:${timePicker.minute}"
+//                Toast.makeText(this@MainActivity, date, Toast.LENGTH_SHORT).show()
+//            }
+//
+//
+//        })
+//    }
+//
+//    private fun basicDialog() {
+//        val basicDialog = BasicDialog(this)
+//        basicDialog.createDialog(
+//            title = "Lorem ipsum?",
+//            mainTxt = "Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
+//                    "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+//            okBtnTxt = "Agree",
+//            cancelBtnTxt = "Disagree"
+//        )
+//        basicDialog.show(supportFragmentManager, "basic_dialog")
+//        basicDialog.setOnButtonClickListener(object : BasicDialog.BasicDialogListeners {
+//            override fun okButtonClicked() {
+//                Toast.makeText(this@MainActivity, "DISAGREE", Toast.LENGTH_SHORT).show()
+//
+//            }
+//
+//            override fun cancelButtonClicked() {
+//                Toast.makeText(this@MainActivity, "AGREE", Toast.LENGTH_SHORT).show()
+//
+//            }
+//
+//        })
+//    }
+//
+//    private fun openCheckBoxDialog() {
+//        val list = arrayListOf("Twitter", "Google", "Instagram", "Facebook")
+//
+//        val checkBoxDialog = CheckBoxDialog(list, "Social medias")
+//        checkBoxDialog.setOnChooseClickListener(object : CheckBoxDialog.OnChooseClickListener {
+//            override fun chooseClick(chosenList: List<String>) {
+//                val stringBuilder = StringBuilder()
+//                for (i in list) {
+//                    stringBuilder.append("$i ")
+//                }
+//                Toast.makeText(this@MainActivity, stringBuilder.toString(), Toast.LENGTH_SHORT)
+//                    .show()
+//            }
+//
+//        })
+//        checkBoxDialog.show(supportFragmentManager, "checkbox_dialog")
+//    }
 
 }
